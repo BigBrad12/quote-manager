7b98ff931dd7f1649397cfdde9e93095ce8874a0
Bradley Slater <bradslater12@gmail.com>
2023-01-08 20:48:51 +0200

    tests, model, front-end, migration, simpleformgem, sendgrid gem

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,41 @@
+class QuotesController < ApplicationController
+
+  before_action :set_quote, only: [:show, :update, :edit, :destroy]
+  def index
+    @quotes = Quote.all
+  end
+  def new
+    @quote = Quote.new 
+  end
+  def edit
+
+  end
+  def update
+    if @quote = Quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was successfully updated"
+    else 
+      render :edit
+    end
+  end
+  def show 
+  end 
+  def create
+    @quote = Quote.create
+    if @quote.save
+      redirect_to quotes_path, notice: "Quote was successfully created"
+    else
+      render :new
+    end
+  end
+  def destroy 
+    @quote.destroy
+    redirect_to quotes_path, notice: "Quote was successfully destroyed"
+  end
+  private
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+  def quote_params 
+    params.require(:quote).permit(:name)
+  end
+end
